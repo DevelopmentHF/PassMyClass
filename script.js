99964be96eadf7bd191de1cb736994ab4bafb52b
developmentHF <development.henry@gmail.com>
2023-05-15 18:23:08 +1000

    check if inputted weights sum to 100

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function addAssToForm() {
 
     const newScoreField = document.createElement('input');
     newScoreField.setAttribute('type','number');
-    newScoreField.setAttribute('name','weight');
+    newScoreField.setAttribute('name','score');
     newScoreField.setAttribute('placeholder','30');
     newAss.appendChild(newScoreField);
 
@@ -27,4 +27,44 @@ function removeAssFromForm() {
     } else {
         console.log("there must be at least one assessment per subject");
     }
+}
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (event) => {
+    // handle the form data
+    event.preventDefault();
+
+    // validate the form to have correct input values
+    validateForm(form);
+
+});
+
+function validateForm(inputForm) {
+    var isErrorPresent = false;
+
+    // weights of assessments must sum to 100% in total
+    if (DoWeightsSumTo100(inputForm.elements['weight']) === false) {
+        console.log("weights do not equal 100");
+        isErrorPresent = true;
+    };
+
+    return isErrorPresent;
+}
+
+// checks if the values of the array `weights` sum to 100%
+function DoWeightsSumTo100(weights) {
+    runningTotal = 0;
+
+    if (weights.length > 1) {
+        weights.forEach(weight => {
+            runningTotal += Number(weight.value);
+        });
+    } else {
+        // case where only one assessment is in the form when submitted
+        // because it is not an array when there is only 1 for some reason
+        runningTotal = Number(weights.value);
+    }
+
+    return (runningTotal === 100);
 }
