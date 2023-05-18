486655437accd69eba79e3c446fd92785e14b7f3
developmentHF <development.henry@gmail.com>
2023-05-18 17:16:37 +1000

    get final score from form into calculation

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 const formfield = document.getElementById('formfield');
 
+// adds a known / estimated assignment to the Dom 
+// includes a weight and score section
 function addAssToForm() {
     const newAss = document.createElement('div');
     newAss.setAttribute('class', 'assessment');
-
+    
+    // adds a weighting field -> i.e this assignment worth 30%
     const newWeightField = document.createElement('input');
     newWeightField.setAttribute('type','number');
     newWeightField.setAttribute('name','weight');
     newWeightField.setAttribute('placeholder','30');
     newAss.appendChild(newWeightField);
-
+    
+    // adds a score field -> i.e you scored 75% on assessment 1
     const newScoreField = document.createElement('input');
     newScoreField.setAttribute('type','number');
     newScoreField.setAttribute('name','score');
     newScoreField.setAttribute('placeholder','30');
     newAss.appendChild(newScoreField);
 
+    // appends to the bottom of the form
     formfield.appendChild(newAss);
 }
 
+// Used to add the 'unknown' assignment to the Dom 
+// it skips out on adding a result input area 
 function addUnknownAssToForm() {
     const newAss = document.createElement('div');
     newAss.setAttribute('class', 'assessment');
 
+    // adds a weighting field -> i.e this assignment worth 30%
     const newWeightField = document.createElement('input');
     newWeightField.setAttribute('type','number');
     newWeightField.setAttribute('name','weight');
     newWeightField.setAttribute('placeholder','30');
     newAss.appendChild(newWeightField);
 
+    // appends to the bottom of the form 
     formfield.appendChild(newAss);
 }
 
+// removes the last assessment from the bottom of the list
 function removeAssFromForm() {
     const assessments = formfield.getElementsByClassName('assessment');
 
@@ -44,6 +54,8 @@ function removeAssFromForm() {
 
 const form = document.getElementById('form')
 
+// acts as sort of a `main function` as everything occurs
+// when the button is submitted
 form.addEventListener('submit', (event) => {
     // handle the form data
     event.preventDefault();
@@ -51,6 +63,8 @@ form.addEventListener('submit', (event) => {
     // validate the form to have correct input values
     validateForm(form);
 
+    // calculate the required score of the unknown assessment mark
+    calculate(form);
 });
 
 function validateForm(inputForm) {
@@ -67,7 +81,7 @@ function validateForm(inputForm) {
 
 // checks if the values of the array `weights` sum to 100%
 function DoWeightsSumTo100(weights) {
-    runningTotal = 0;
+    let runningTotal = 0;
 
     if (weights.length > 1) {
         weights.forEach(weight => {
@@ -81,3 +95,15 @@ function DoWeightsSumTo100(weights) {
 
     return (runningTotal === 100);
 }
+
+// returns a required percentage of unknown assessment to achieve the final percentage
+function calculate(inputForm) {
+    let requiredScore = inputForm.elements['final-mark']; 
+    console.log(requiredScore.value);
+    // basic eq with one variable 
+    // weight * score + weight * score etc etc = final percentage
+    console.log(document.getElementsByClassName("assessment")); 
+    // read in what the final score is
+
+    return requiredScore;
+}  
